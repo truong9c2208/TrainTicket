65687aeed9502b05014815a453deaa7a93fc133c
Shibaa <truong9c2208@gmail.com>
2026-04-24 12:54:57 +0700

    feat: implement JWT strategy for Passport authentication in backend

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -22,7 +22,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   validate(payload: JwtPayload): AuthUser {
     return {
-      userId: payload.sub,
+      userId: Number(payload.sub),
       email: payload.email,
       role: payload.role,
     };
